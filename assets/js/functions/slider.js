d80db270a5cade056884c5a1f813d6fc1e57364d
gassdev <gassdev@gmail.com>
2021-10-30 20:29:12 +0000

    create pagination dots

--- assets/js/functions/slider.js
+++ assets/js/functions/slider.js
@@ -1,5 +1,6 @@
 var hideSlide = () => {
   var slides = document.querySelectorAll('.slide')
+  displayDot(slides)
   for (let index = 1; index < slides.length; index++) {
     const slide = slides[index]
     slide.style.display = 'none'
@@ -8,4 +9,14 @@ var hideSlide = () => {
 
 var initSlider = () => {
   hideSlide()
+}
+
+var displayDot = (sliders) => {
+  for (let index = 0; index < sliders.length; index++) {
+    const slide = sliders[index]
+    var span = document.createElement('span')
+    // span.className = 'dot'
+    span.setAttribute('class', 'dot')
+    document.getElementById('dots').appendChild(span)
+  }
 }
